099c799d898017f259fa04617f7f6881c9e79477
Jeff Chang <jeffjmc4@gmail.com>
2023-02-28 18:50:00 -0800

    completed conference new

--- ghi/attendees/src/App.js
+++ ghi/attendees/src/App.js
@@ -1,31 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
+import AttendeesList from './AttendeesList';
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm';
+import React from 'react';
 
 function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
+
   return (
     <>
       <Nav />
-
       <div className="container">
-        <table className='table table-striped table-hover'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Conference</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.attendees.map(attendee => {
-              return (
-                <tr key={attendee.href}>
-                  <td>{attendee.name}</td>
-                  <td>{attendee.conference}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <ConferenceForm />
+        {/* <LocationForm /> */}
+        {/* <AttendeesList attendees={props.attendees} /> */}
       </div>
     </>
   );
