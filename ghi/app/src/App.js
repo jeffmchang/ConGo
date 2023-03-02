593cb72e2dd2076d3d0adca5ae399ebf4928c30d
Jeff Chang <jeffjmc4@gmail.com>
2023-03-01 16:58:36 -0800

    many requests

---
+++ ghi/app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav';
+import AttendeesList from './AttendeesList';
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm';
+import AttendForm from './AttendForm';
+import PresentationForm from './PresentationForm';
+import MainPage from './MainPage';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
+
+  return (
+
+    <BrowserRouter>
+      <Nav />
+      <div className="my-5 container">
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="attendees">
+            <Route path="new" element={<AttendForm />} />
+            <Route path="" element={<AttendeesList attendees={props.attendees} />} />
+          </Route>
+          <Route path="conferences">
+            <Route path="new" element={<ConferenceForm />} />
+          </Route>
+          <Route path="locations">
+            <Route path="new" element={<LocationForm />} />
+          </Route>
+          <Route path="presentations">
+            <Route path="new" element={<PresentationForm />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
